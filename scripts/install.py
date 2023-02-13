accbbd259c3a3a95861860b25eb1ade05b51163e
fropych <fropych@gmail.com>
2023-02-13 23:48:50 +0300

    fix

--- scripts/install.py
+++ scripts/install.py
@@ -1,6 +1,6 @@
 import launch
 
 if not launch.is_installed("litemapy"):
-    launch.run_pip("install litemapy", "requirements for Mine Diffusion")
+    launch.run_pip("install litemapy==0.7.2b0", "requirements for Mine Diffusion")
 if not launch.is_installed("numba"):
-    launch.run_pip("install litemapy", "requirements for Mine Diffusion")
+    launch.run_pip("install numba==0.56.4", "requirements for Mine Diffusion")
